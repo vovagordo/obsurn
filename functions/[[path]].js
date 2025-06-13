5655da53a2d83b0d0f737e80d317f83f6784dafe
vovagordo <denissemenkov26@gmail.com>
2025-06-13 12:57:38 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'voloner.com.ua'
+	const proxyUrl = 'https://voloner.com.ua'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
